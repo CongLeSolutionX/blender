2c22fd7e1366ce06da908b756fff0fcb76d3092e
Falk David <falk@blender.org>
2024-10-16 18:09:23 +0200

    Fix: GPv3: Reduce drawing performance regression
    During the painting operation, the drawing is updated in-place.
    Previously, we simply tagged the whole drawing for a topology update.
    This meant that the triangulation and curve normals were recomputed
    for (pretty much) every new input sample.
    
    Since the draw tool only creates one new stroke, we can copy the
    triangulation of all the other strokes and only recompute the triangulation
    of the newly drawn stroke(s).
    
    First, a new `triangle_offsets_cache` is added that is lazily computed.
    The computation for this should be pretty cheap.
    Then, a new function `Drawing::tag_topology_changed(const IndexMask &curves_to_update)`
    is added. This function takes an index mask of curves and only updates
    their triangle cache. The cache for the other curves is copied.
    
    In a test file with around 400k points, recomputing the triangle cache
    (every input sample) took around 45ms. With this patch, the time for
    each sample goes down to less than 0.1ms.
    
    But, drawing a longer and longer stroke still makes this time go up,
    because the triangulation for the single stroke becomes more and
    more expensive. This will have to be improved further.

--- source/blender/blenkernel/BKE_grease_pencil.hh
+++ source/blender/blenkernel/BKE_grease_pencil.hh
@@ -52,6 +52,10 @@ constexpr float LEGACY_RADIUS_CONVERSION_FACTOR = 1.0f / 2000.0f;
 class DrawingRuntime {
  public:
   /**
+   * Triangle offset cache for all the strokes in the drawing.
+   */
+  mutable SharedCache<Vector<int>> triangle_offsets_cache;
+  /**
    * Triangle cache for all the strokes in the drawing.
    */
   mutable SharedCache<Vector<uint3>> triangles_cache;
@@ -87,16 +91,21 @@ class Drawing : public ::GreasePencilDrawing {
   const bke::CurvesGeometry &strokes() const;
   bke::CurvesGeometry &strokes_for_write();
   /**
-   * The triangles for all the fills in the geometry.
+   * The triangles for fill geometry. Grouped by each stroke.
    */
-  Span<uint3> triangles() const;
+  GroupedSpan<uint3> triangles() const;
   /**
    * Normal vectors for a plane that fits the stroke.
    */
   Span<float3> curve_plane_normals() const;
+  
   void tag_texture_matrices_changed();
+
   void tag_positions_changed();
+  void tag_positions_changed(const IndexMask &curves_to_update);
+
   void tag_topology_changed();
+  void tag_topology_changed(const IndexMask &curves_to_update);
 
   /**
    * Returns the matrices that transform from a 3D point in layer-space to a 2D point in
@@ -159,6 +168,12 @@ class Drawing : public ::GreasePencilDrawing {
    * Return the number of users (keyframes) of this drawing.
    */
   int user_count() const;
+
+ private:
+  /**
+   * The offset indices for each stroke in the flat triangle cache.
+   */
+  OffsetIndices<int> triangle_offsets() const;
 };
 static_assert(sizeof(Drawing) == sizeof(::GreasePencilDrawing));
 
