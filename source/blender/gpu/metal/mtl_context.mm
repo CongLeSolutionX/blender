ad72aa33044139967ee7a6ed7cf93044bbe81d8b
James McCarthy <jamesmccarthy@apple.com>
2024-10-16 18:05:03 +0100

    Fixed formatting.

--- source/blender/gpu/metal/mtl_context.mm
+++ source/blender/gpu/metal/mtl_context.mm
@@ -2733,7 +2733,7 @@ void present(MTLRenderPassDescriptor *blit_descriptor,
   [cmdbuf addCompletedHandler:^(id<MTLCommandBuffer> /*cb*/) {
     /* Flag freed buffers associated with this CMD buffer as ready to be freed. */
     cmd_free_buffer_list->decrement_reference();
-    
+
     /* Decrement count */
     MTLCommandBufferManager::num_active_cmd_bufs--;
     MTL_LOG_INFO("Active command buffers: %d", MTLCommandBufferManager::num_active_cmd_bufs);
