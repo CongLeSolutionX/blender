27c03776ebab8abd56e9e184927bc0f3c55d8035
Clément Foucault <foucault.clem@gmail.com>
2024-10-21 22:26:52 +0200

    GPU: Move Polyline shader to primitive expansion
    This port is not so straightforward.
    
    This shader is used in different configurations and is
    available to python bindings. So we need to keep
    compatibility with different attributes configurations.
    
    This is why attributes are loaded per component and a
    uniform sets the length of the component.
    
    Since this shader can be used from both the imm and batch
    API, we need to inject some workarounds to bind the buffers
    correctly.
    
    Rel #127493

--- source/blender/gpu/shaders/gpu_shader_3D_polyline_frag.glsl
+++ source/blender/gpu/shaders/gpu_shader_3D_polyline_frag.glsl
@@ -7,14 +7,13 @@
 void main()
 {
 #ifdef CLIP
-  if (interp.clip < 0.0) {
+  if (clip < 0.0) {
     discard;
   }
 #endif
-  fragColor = interp.final_color;
+  fragColor = final_color;
   if (lineSmooth) {
-    fragColor.a *= clamp(
-        (lineWidth + SMOOTH_WIDTH) * 0.5 - abs(interp_noperspective.smoothline), 0.0, 1.0);
+    fragColor.a *= clamp((lineWidth + SMOOTH_WIDTH) * 0.5 - abs(smoothline), 0.0, 1.0);
   }
   fragColor = blender_srgb_to_framebuffer_space(fragColor);
 }
