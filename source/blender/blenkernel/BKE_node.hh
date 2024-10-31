6e0607f14f87eddab43e1f3ace1b42b5e195b505
SensArice <sensarice@noreply.localhost>
2024-10-31 22:19:07 +0100

    更新 source/blender/blenkernel/BKE_node.hh

--- source/blender/blenkernel/BKE_node.hh
+++ source/blender/blenkernel/BKE_node.hh
@@ -1432,6 +1432,7 @@ void node_tree_remove_layer_n(bNodeTree *ntree, Scene *scene, int layer_index);
 #define FN_NODE_MATRIX_DETERMINANT 1247
 #define FN_NODE_STRING_KEYWORD_COUNT 1248
 #define FN_NODE_STRING_SELECT_LINE 1249
+#define FN_NODE_STRING_FIND 1250
 /** \} */
 
 void node_system_init();
