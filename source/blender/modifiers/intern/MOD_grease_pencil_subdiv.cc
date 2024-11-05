551a34cc737edded3a55f307b7a2bd67c4a02f69
YimingWu <xp8110@outlook.com>
2024-11-05 20:45:18 +0800

    Support catmull subdiv

--- source/blender/modifiers/intern/MOD_grease_pencil_subdiv.cc
+++ source/blender/modifiers/intern/MOD_grease_pencil_subdiv.cc
@@ -86,7 +86,8 @@ static void subdivide_drawing(ModifierData &md, Object &ob, bke::greasepencil::D
       &ob, drawing.strokes_for_write(), mmd.influence, memory);
 
   if (use_catmull_clark) {
-    bke::CurvesGeometry subdivided_curves = drawing.strokes();
+    bke::CurvesGeometry subdivided_curves = modifier::greasepencil::convert_to_poly_curves(
+        drawing.strokes_for_write());
     for ([[maybe_unused]] const int level_i : IndexRange(mmd.level)) {
       VArray<int> one_cut = VArray<int>::ForSingle(1, subdivided_curves.points_num());
       subdivided_curves = geometry::subdivide_curves(
