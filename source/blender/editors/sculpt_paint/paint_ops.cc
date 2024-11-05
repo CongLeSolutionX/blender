48338ee906c12fbe55fc584870664e8f7f657f0b
Lukas Tönne <lukas@blender.org>
2024-11-05 11:45:47 +0100

    Support the `VertexGPencil` paint mode when adding palette colors.

--- source/blender/editors/sculpt_paint/paint_ops.cc
+++ source/blender/editors/sculpt_paint/paint_ops.cc
@@ -185,7 +185,8 @@ static int palette_color_add_exec(bContext *C, wmOperator * /*op*/)
              PaintMode::Texture2D,
              PaintMode::Vertex,
              PaintMode::Sculpt,
-             PaintMode::GPencil))
+             PaintMode::GPencil,
+             PaintMode::VertexGPencil))
     {
       copy_v3_v3(color->rgb, BKE_brush_color_get(scene, paint, brush));
       color->value = 0.0;
