zyxcambridge/blender_llm

Fix #127399: Sculpt temporary Smooth brush ignores some settings
If the Smooth brush (which is switched to from other brushes holding
`Shift`) had the following settings (possibly others as well)
- Stabilize Stroke
- Airbrush
- Line
These were ignored (not drawing their correct cursor, airbrush not
working because the timer wasnt started).

Problem here is that `smooth_brush_toggle_on` (called via stroke->
test_start) changes the brush, this was ignored in following code (and
in case of the `Stabilize Stroke` is called only after the code that was
checking if this was ON).

So to resolve, get the brush again after `stroke->test_start` has run
(and also move the check `Stabilize Stroke` a bit (to  place that
actually fits much better since similar things like line visualization
are handled there as well)

Pull Request: https://projects.blender.org/blender/blender/pulls/127412

source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -1512,11 +1512,6 @@ int paint_stroke_modal(bContext *C, wmOperator *op, const wmEvent *event, PaintS
       return OPERATOR_FINISHED;
     }
 
-    if (paint_supports_smooth_stroke(stroke, *br, mode)) {
-      stroke->stroke_cursor = WM_paint_cursor_activate(
-          SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_tool_poll, paint_draw_smooth_cursor, stroke);
-    }
-
     stroke->stroke_init = true;
     first_modal = true;
   }
@@ -1533,6 +1528,15 @@ int paint_stroke_modal(bContext *C, wmOperator *op, const wmEvent *event, PaintS
     stroke->stroke_started = stroke->test_start(C, op, sample_average.mouse);
 
     if (stroke->stroke_started) {
+      /* StrokeTestStart often updates the currently active brush so we need to re-retrieve it
+       * here. */
+      br = BKE_paint_brush(paint);
+
+      if (paint_supports_smooth_stroke(stroke, *br, mode)) {
+        stroke->stroke_cursor = WM_paint_cursor_activate(
+            SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_tool_poll, paint_draw_smooth_cursor, stroke);
+      }
+
       if (br->flag & BRUSH_AIRBRUSH) {
         stroke->timer = WM_event_timer_add(
             CTX_wm_manager(C), CTX_wm_window(C), TIMER, stroke->brush->rate);

CongLeSolutionX/blender

source/blender/editors/sculpt_paint/paint_stroke.cc
@@ -1495,11 +1495,6 @@ int paint_stroke_modal(bContext *C, wmOperator *op, const wmEvent *event, PaintS
       return OPERATOR_FINISHED;
     }
 
-    if (paint_supports_smooth_stroke(stroke, *br, mode)) {
-      stroke->stroke_cursor = WM_paint_cursor_activate(
-          SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_tool_poll, paint_draw_smooth_cursor, stroke);
-    }
-
     stroke->stroke_init = true;
     first_modal = true;
   }
@@ -1516,6 +1511,15 @@ int paint_stroke_modal(bContext *C, wmOperator *op, const wmEvent *event, PaintS
     stroke->stroke_started = stroke->test_start(C, op, sample_average.mouse);
 
     if (stroke->stroke_started) {
+      /* StrokeTestStart often updates the currently active brush so we need to re-retrieve it
+       * here. */
+      br = BKE_paint_brush(p);
+
+      if (paint_supports_smooth_stroke(stroke, *br, mode)) {
+        stroke->stroke_cursor = WM_paint_cursor_activate(
+            SPACE_TYPE_ANY, RGN_TYPE_ANY, paint_brush_tool_poll, paint_draw_smooth_cursor, stroke);
+      }
+
       if (br->flag & BRUSH_AIRBRUSH) {
         stroke->timer = WM_event_timer_add(
             CTX_wm_manager(C), CTX_wm_window(C), TIMER, stroke->brush->rate);