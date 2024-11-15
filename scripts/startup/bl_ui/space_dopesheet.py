ffeb6cc7eb2c88c6aa8535f06d75c7f123de4793
Pratik Borhade <pratikborhade302@gmail.com>
2024-11-15 15:02:51 +0530

    GPv3: Adjustment panel in dopesheet
    Missed in b4881f6b0afc74bceba3821ff6824899c9d99132
    Expose adjustment panel in Grease pencil dopesheet side panel as
    done for others in 6e137f957f59fabc07f5a030118d1f1bf9ca9aa9

--- scripts/startup/bl_ui/space_dopesheet.py
+++ scripts/startup/bl_ui/space_dopesheet.py
@@ -13,6 +13,7 @@ from bl_ui.properties_data_grease_pencil import (
     GreasePencil_LayerMaskPanel,
     GreasePencil_LayerTransformPanel,
     GreasPencil_LayerRelationsPanel,
+    GreasePencil_LayerAdjustmentsPanel,
 )
 
 from rna_prop_ui import PropertyPanel
@@ -960,6 +961,13 @@ class DOPESHEET_PT_grease_pencil_layer_relations(
     bl_parent_id = "DOPESHEET_PT_grease_pencil_mode"
     bl_options = {'DEFAULT_CLOSED'}
 
+class DOPESHEET_PT_grease_pencil_layer_adjustments(
+        GreasePencilLayersDopeSheetPanel,
+        GreasePencil_LayerAdjustmentsPanel,
+        Panel):
+    bl_label = "Adjustments"
+    bl_parent_id = "DOPESHEET_PT_grease_pencil_mode"
+    bl_options = {'DEFAULT_CLOSED'}
 
 classes = (
     DOPESHEET_HT_header,
@@ -986,6 +994,7 @@ classes = (
     DOPESHEET_PT_grease_pencil_mode,
     DOPESHEET_PT_grease_pencil_layer_masks,
     DOPESHEET_PT_grease_pencil_layer_transform,
+    DOPESHEET_PT_grease_pencil_layer_adjustments,
     DOPESHEET_PT_grease_pencil_layer_relations,
 )
 
