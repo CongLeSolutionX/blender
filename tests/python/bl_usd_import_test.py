e2b8dac73274689c320991d8ad0f239c88971a63
Charles Flèche <charles.fleche@ubisoft.com>
2024-11-20 16:08:04 -0500

    Test against usd_name_property_template.usda

--- tests/python/bl_usd_import_test.py
+++ tests/python/bl_usd_import_test.py
@@ -1415,56 +1415,13 @@ class USDImportTest(AbstractUSDTest):
         check_materials()
 
     def test_import_object_and_data_name_properties(self):
-        src = """#usda 1.0
-
-def Xform "XformThenCube"
-{
-  def Cube "Cube"
-  {
-  }
-}
-
-def Xform "XformThenXformCube"
-{
-  def Xform "XformIntermediate"
-  {
-    def Mesh "Cube" (
-      prepend apiSchemas = ["MaterialBindingAPI"]
-    )
-    {
-      int[] faceVertexCounts = [4, 4, 4, 4, 4, 4]
-      int[] faceVertexIndices = [0, 4, 6, 2, 3, 2, 6, 7, 7, 6, 4, 5, 5, 1, 3, 7, 1, 0, 2, 3, 5, 4, 0, 1]
-      point3f[] points = [(1, 1, 1), (1, 1, -1), (1, -1, 1), (1, -1, -1), (-1, 1, 1), (-1, 1, -1), (-1, -1, 1), (-1, -1, -1)]
-
-      rel material:binding = </Material>
-    }
-  }
-}
-
-def Cube "Cube"
-{
-}
-
-def Material "Material"
-{
-  token outputs:surface.connect = </_materials/Material/Principled_BSDF.outputs:surface>
-
-  def Shader "Principled_BSDF"
-  {
-    uniform token info:id = "UsdPreviewSurface"
-  }
-}
-"""
-        src_path = self.tempdir / "src.usda"
-        src_path.write_text(src)
-
         tmp_path = str(self.tempdir / "tmp.usda")
 
         old = SaveStageUsdImportHook.path
         SaveStageUsdImportHook.path = tmp_path
         bpy.utils.register_class(SaveStageUsdImportHook)
 
-        bpy.ops.wm.usd_import(filepath=str(src_path))
+        bpy.ops.wm.usd_import(filepath=str(self.testdir / "usd_name_property_template.usda"))
 
         stage = Usd.Stage.Open(tmp_path)
 
