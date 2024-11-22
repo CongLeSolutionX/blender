fb2e102119eb39e3a571c41e40b30f9251b041c0
Clément Foucault <foucault.clem@gmail.com>
2024-11-22 11:30:05 +0100

    Overlay-Next: Add base class for documentation

---
+++ source/blender/draw/engines/overlay/overlay_next_base.hh
@@ -0,0 +1,34 @@
+/* SPDX-FileCopyrightText: 2024 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup overlay
+ */
+
+#pragma once
+
+#include "overlay_next_private.hh"
+
+namespace blender::draw::overlay {
+
+class Overlay {
+  virtual void begin_sync(Resources &res, const State &state, const View &view) = 0;
+
+  virtual void object_sync(Manager &manager,
+                           const ObjectRef &ob_ref,
+                           const State &state,
+                           Resources &res) = 0;
+
+  virtual void edit_object_sync(Manager &manager,
+                                const ObjectRef &ob_ref,
+                                const State &state,
+                                Resources &res) = 0;
+
+  virtual void draw(Framebuffer &framebuffer, Manager &manager, View &view) = 0;
+
+  virtual void draw_color_only(Framebuffer &framebuffer, Manager &manager, View &view) = 0;
+  virtual void draw_on_render(GPUFrameBuffer *framebuffer, Manager &manager, View &view) = 0;
+};
+
+}  // namespace blender::draw::overlay
