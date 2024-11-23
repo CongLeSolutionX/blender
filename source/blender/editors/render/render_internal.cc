CongLeSolutionX/blender

commit 5ecb70964ed7006ba236875a4feb1bfa773ea9d4

Fix #125490: Scene strip incorrectly rendered as solid view.
Prevent VSE cache returning image when running render job.

Previously, before render job was started, cache was cleared, but since
disk cache is not cleared, it returned an image. Now cache is not
cleared, but it does not return or store images when rendering.

Pull Request: https://projects.blender.org/blender/blender/pulls/125947

source/blender/editors/render/render_internal.cc
@@ -1024,12 +1024,6 @@ static int screen_render_invoke(bContext *C, wmOperator *op, const wmEvent *even
   /* flush sculpt and editmode changes */
   ED_editors_flush_edits_ex(bmain, true, false);
 
-  /* cleanup sequencer caches before starting user triggered render.
-   * otherwise, invalidated cache entries can make their way into
-   * the output rendering. We can't put that into RE_RenderFrame,
-   * since sequence rendering can call that recursively... (peter) */
-  SEQ_cache_cleanup(scene);
-
   /* store spare
    * get view3d layer, local layer, make this nice api call to render
    * store spare */

source/blender/sequencer/intern/image_cache.cc
@@ -746,7 +746,7 @@ void seq_cache_thumbnail_cleanup(Scene *scene, rctf *r_view_area_safe)
 ImBuf *seq_cache_get(const SeqRenderData *context, Sequence *seq, float timeline_frame, int type)
 {
 
-  if (context->skip_cache || context->is_proxy_render || !seq) {
+  if (context->skip_cache || context->is_proxy_render || context->for_render || !seq) {
     return nullptr;
   }
 
@@ -866,7 +866,7 @@ void seq_cache_thumbnail_put(const SeqRenderData *context,
 void seq_cache_put(
     const SeqRenderData *context, Sequence *seq, float timeline_frame, int type, ImBuf *i)
 {
-  if (i == nullptr || context->skip_cache || context->is_proxy_render || !seq) {
+  if (i == nullptr || context->skip_cache || context->is_proxy_render || context->for_render || !seq) {
     return;
   }
 

commit 63dd20cf4a17798805a5545ab775c02265f7620e

Fix #130742 Scene render from VSE is taking twice as long
Regression caused by 5ecb70964e.

Instead of disabling cache completely during rendering, flag cache
entries as temporary. When reading entries also skip disk cache, since
in 5ecb70964e goal was to avoid reading from disk cache.

source/blender/editors/render/render_internal.cc
@@ -1024,6 +1024,9 @@ static int screen_render_invoke(bContext *C, wmOperator *op, const wmEvent *even
   /* flush sculpt and editmode changes */
   ED_editors_flush_edits_ex(bmain, true, false);
 
+  /* Cleanup VSE cache since it is not guaranteed, that images can not be invalid. */
+  SEQ_cache_cleanup(scene);
+
   /* store spare
    * get view3d layer, local layer, make this nice api call to render
    * store spare */

source/blender/sequencer/intern/image_cache.cc
@@ -687,7 +687,7 @@ void seq_cache_cleanup_sequence(Scene *scene,
 ImBuf *seq_cache_get(const SeqRenderData *context, Sequence *seq, float timeline_frame, int type)
 {
 
-  if (context->skip_cache || context->is_proxy_render || context->for_render || !seq) {
+  if (context->skip_cache || context->is_proxy_render || !seq) {
     return nullptr;
   }
 
@@ -721,6 +721,10 @@ ImBuf *seq_cache_get(const SeqRenderData *context, Sequence *seq, float timeline
 
   if (ibuf) {
     return ibuf;
+  }
+
+  if (context->for_render) {
+    return nullptr;
   }
 
   /* Try disk cache: */
@@ -776,9 +780,7 @@ bool seq_cache_put_if_possible(
 void seq_cache_put(
     const SeqRenderData *context, Sequence *seq, float timeline_frame, int type, ImBuf *i)
 {
-  if (i == nullptr || context->skip_cache || context->is_proxy_render || context->for_render ||
-      !seq)
-  {
+  if (i == nullptr || context->skip_cache || context->is_proxy_render || !seq) {
     return;
   }
 
@@ -807,6 +809,10 @@ void seq_cache_put(
   SeqCacheKey *key = seq_cache_allocate_key(cache, context, seq, timeline_frame, type);
   seq_cache_put_ex(scene, key, i);
   seq_cache_unlock(scene);
+
+  if (context->for_render) {
+    key->is_temp_cache = true;
+  }
 
   if (!key->is_temp_cache) {
     if (seq_disk_cache_is_enabled(context->bmain)) {