46f3c42ce269d656a165412f27d4e56646a73c7c
Campbell Barton <campbell@blender.org>
2024-11-25 12:15:59 +1100

    Fix #130788: Select edge loops doesn't refresh
    Regression in [0] missed setting "changed".
    
    [0]: 093a3abbd42d978dec25d9255b5e08cfd2a0c6fd

--- source/blender/editors/mesh/editmesh_select.cc
+++ source/blender/editors/mesh/editmesh_select.cc
@@ -1612,7 +1612,7 @@ static int edbm_loop_multiselect_exec(bContext *C, wmOperator *op)
           walker_select(em, BMW_EDGELOOP_NONMANIFOLD, eed, true);
         }
         else {
-          walker_select(em, BMW_EDGELOOP, eed, true);
+          changed |= walker_select(em, BMW_EDGELOOP, eed, true);
         }
       }
       EDBM_selectmode_flush(em);
