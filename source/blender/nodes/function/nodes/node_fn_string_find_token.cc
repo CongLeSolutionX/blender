848ea65ca48a6958959655e6d4c369a2b84be215
SensArice <sensmd@qq.com>
2024-11-28 00:09:23 +0800

    USE CONTINUE

--- source/blender/nodes/function/nodes/node_fn_string_find_token.cc
+++ source/blender/nodes/function/nodes/node_fn_string_find_token.cc
@@ -17,6 +17,38 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Int>("Next Find").min(0).default_value(1);
   b.add_output<decl::Int>("Token Position");
 }
+class char_length {
+ public:
+  static constexpr int array[5] = {0, 0x7F, 0xDF, 0xEF, 0xF7};
+  static int to_int(int length)
+  {
+    return array[length];
+  }
+  static int to_int(char c)
+  {
+    unsigned char uc;
+    if (uc <= array[1]) {
+      return 1;
+    }
+    else if (uc <= array[2]) {
+      return 2;
+    }
+    else if (uc <= array[3]) {
+      return 3;
+    }
+    else if (uc <= array[4]) {
+      return 4;
+    }
+    else {
+      return -1;
+    }
+  }
+  static bool equal(char c, int length)
+  {
+    return to_int(c) == length;
+  }
+};
+
 std::u32string u32_from_utf8(const StringRef &utf8)
 {
   std::u32string u32;
@@ -26,40 +58,46 @@ std::u32string u32_from_utf8(const StringRef &utf8)
   const char *from_end = from + utf8.size();
 
   while (from < from_end) {
-    unsigned char lead = static_cast<unsigned char>(*from);
+    const unsigned char lead = static_cast<unsigned char>(*from);
+    const int lead_int = static_cast<int>(static_cast<unsigned char>(*from));
     char32_t code_point = 0;
-
-    if (lead <= 0x7F) {
+    if (lead_int <= char_length::to_int(1)) {
       code_point = lead;
       from++;
     }
-    else if (lead <= 0xDF && from + 1 < from_end) {
-      unsigned char second = static_cast<unsigned char>(from[1]);
-      if ((second & 0xC0) == 0x80) {
-        code_point = ((lead & 0x1F) << 6) | (second & 0x3F);
-        from += 2;
+    else if (lead_int <= char_length::to_int(2) && from + 1 < from_end) {
+      const unsigned char c1 = static_cast<unsigned char>(from[1]);
+      if ((c1 & 0xC0) != 0x80) {
+        from++;
+        continue;
       }
+      code_point = ((lead & 0x1F) << 6) | (c1 & 0x3F);
+      from += 2;
     }
-    else if (lead <= 0xEF && from + 2 < from_end) {
-      unsigned char second = static_cast<unsigned char>(from[1]);
-      unsigned char third = static_cast<unsigned char>(from[2]);
-      if ((second & 0xC0) == 0x80 && (third & 0xC0) == 0x80) {
-        code_point = ((lead & 0x0F) << 12) | ((second & 0x3F) << 6) | (third & 0x3F);
-        from += 3;
+    else if (lead_int <= char_length::to_int(3) && from + 2 < from_end) {
+      const unsigned char c1 = static_cast<unsigned char>(from[1]);
+      const unsigned char c2 = static_cast<unsigned char>(from[2]);
+      if ((c1 & 0xC0) != 0x80 && (c2 & 0xC0) != 0x80) {
+        from++;
+        continue;
       }
+      code_point = ((lead & 0x0F) << 12) | ((c1 & 0x3F) << 6) | (c2 & 0x3F);
+      from += 3;
     }
-    else if (lead <= 0xF7 && from + 3 < from_end) {
-      unsigned char second = static_cast<unsigned char>(from[1]);
-      unsigned char third = static_cast<unsigned char>(from[2]);
-      unsigned char fourth = static_cast<unsigned char>(from[3]);
-      if ((second & 0xC0) == 0x80 && (third & 0xC0) == 0x80 && (fourth & 0xC0) == 0x80) {
-        code_point = ((lead & 0x07) << 18) | ((second & 0x3F) << 12) | ((third & 0x3F) << 6) |
-                     (fourth & 0x3F);
-        from += 4;
+    else if (lead_int <= char_length::to_int(4) && from + 3 < from_end) {
+      const unsigned char c1 = static_cast<unsigned char>(from[1]);
+      const unsigned char c2 = static_cast<unsigned char>(from[2]);
+      const unsigned char c3 = static_cast<unsigned char>(from[3]);
+      if ((c1 & 0xC0) != 0x80 && (c2 & 0xC0) != 0x80 && (c3 & 0xC0) != 0x80) {
+        from++;
+        continue;
       }
+      code_point = ((lead & 0x07) << 18) | ((c1 & 0x3F) << 12) | ((c2 & 0x3F) << 6) | (c3 & 0x3F);
+      from += 4;
     }
     else {
-      return std::u32string();
+      from++;
+      continue;
     }
     u32.push_back(code_point);
   }
