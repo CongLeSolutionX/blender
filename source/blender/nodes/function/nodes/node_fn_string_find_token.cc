f4f42cf823bc1c3112df1ab7b70a0b1704158c52
SensArice <sensmd@qq.com>
2024-11-28 11:09:45 +0800

    FIND US BLI FUNCTION TO U32 STRING

--- source/blender/nodes/function/nodes/node_fn_string_find_token.cc
+++ source/blender/nodes/function/nodes/node_fn_string_find_token.cc
@@ -17,99 +17,36 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Int>("Next Find").min(0).default_value(1);
   b.add_output<decl::Int>("Token Position");
 }
-class char_length {
- public:
-  static constexpr int array[5] = {0, 0x7F, 0xDF, 0xEF, 0xF7};
-  static int to_int(int length)
-  {
-    return array[length];
-  }
-  static int to_int(char c)
-  {
-    unsigned char uc;
-    if (uc <= array[1]) {
-      return 1;
-    }
-    else if (uc <= array[2]) {
-      return 2;
-    }
-    else if (uc <= array[3]) {
-      return 3;
-    }
-    else if (uc <= array[4]) {
-      return 4;
-    }
-    else {
-      return -1;
-    }
-  }
-  static bool equal(char c, int length)
-  {
-    return to_int(c) == length;
-  }
-};
 
-std::u32string u32_from_utf8(const StringRef &utf8)
+std::u32string bli_str_utf8_as_u32string(const std::string &src_c)
 {
-  std::u32string u32;
-  u32.reserve(utf8.size());
+  const size_t src_c_len = src_c.size();
+  std::u32string result;
+  result.reserve(src_c_len); 
 
-  const char *from = utf8.data();
-  const char *from_end = from + utf8.size();
-
-  while (from < from_end) {
-    const unsigned char lead = static_cast<unsigned char>(*from);
-    const int lead_int = static_cast<int>(static_cast<unsigned char>(*from));
-    char32_t code_point = 0;
-    if (lead_int <= char_length::to_int(1)) {
-      code_point = lead;
-      from++;
+  const char *src_c_end = src_c.data() + src_c_len;
+  size_t index = 0;
+  while (index < src_c_len) {
+    const uint unicode = BLI_str_utf8_as_unicode_step_or_error(src_c.data(), src_c_len, &index);
+    if (unicode != BLI_UTF8_ERR) {
+      result.push_back(unicode);
+    } else {
+      result.push_back('?');
+      const char *src_c_next = BLI_str_find_next_char_utf8(src_c.data() + index, src_c_end);
+      index = size_t(src_c_next - src_c.data());
     }
-    else if (lead_int <= char_length::to_int(2) && from + 1 < from_end) {
-      const unsigned char c1 = static_cast<unsigned char>(from[1]);
-      if ((c1 & 0xC0) != 0x80) {
-        from++;
-        continue;
-      }
-      code_point = ((lead & 0x1F) << 6) | (c1 & 0x3F);
-      from += 2;
-    }
-    else if (lead_int <= char_length::to_int(3) && from + 2 < from_end) {
-      const unsigned char c1 = static_cast<unsigned char>(from[1]);
-      const unsigned char c2 = static_cast<unsigned char>(from[2]);
-      if ((c1 & 0xC0) != 0x80 && (c2 & 0xC0) != 0x80) {
-        from++;
-        continue;
-      }
-      code_point = ((lead & 0x0F) << 12) | ((c1 & 0x3F) << 6) | (c2 & 0x3F);
-      from += 3;
-    }
-    else if (lead_int <= char_length::to_int(4) && from + 3 < from_end) {
-      const unsigned char c1 = static_cast<unsigned char>(from[1]);
-      const unsigned char c2 = static_cast<unsigned char>(from[2]);
-      const unsigned char c3 = static_cast<unsigned char>(from[3]);
-      if ((c1 & 0xC0) != 0x80 && (c2 & 0xC0) != 0x80 && (c3 & 0xC0) != 0x80) {
-        from++;
-        continue;
-      }
-      code_point = ((lead & 0x07) << 18) | ((c1 & 0x3F) << 12) | ((c2 & 0x3F) << 6) | (c3 & 0x3F);
-      from += 4;
-    }
-    else {
-      from++;
-      continue;
-    }
-    u32.push_back(code_point);
   }
-  return u32;
+
+  return result;
 }
+
 static int string_find_token(const StringRef text,
                              const StringRef token,
                              const int start,
                              const int next)
 {
-  std::u32string a_u32 = u32_from_utf8(text);
-  std::u32string b_u32 = u32_from_utf8(token);
+  std::u32string a_u32 = bli_str_utf8_as_u32string(text);
+  std::u32string b_u32 = bli_str_utf8_as_u32string(token);
 
   if (start < 0 || start > a_u32.size()) {
     return -1;
